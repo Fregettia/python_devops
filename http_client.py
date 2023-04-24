05cbc417795d38c7aa17db8718e0d0a2f1b4f93b
tropica <99130969+Fregettia@users.noreply.github.com>
2023-04-24 20:25:42 +0800

    implement other apis

--- http_client.py
+++ http_client.py
@@ -1,14 +1,21 @@
 import json
 import requests
 from requests.exceptions import RequestException
+import sys
+
+server_ip, server_port = sys.argv[1], sys.argv[2]
+input_json, output_json = sys.argv[3], sys.argv[4]
+
 
 def load_input_json(filename):
     with open(filename, "r") as f:
         return json.load(f)
 
+
 def save_output_json(data, filename):
     with open(filename, "w") as f:
         json.dump(data, f, indent=2)
+
 
 def send_request(payload, url):
     try:
@@ -17,14 +24,17 @@ def send_request(payload, url):
     except RequestException as e:
         return {"status": -1, "msg": str(e)}
 
-def main():
-    input_data = load_input_json("input.json")
-    url = "http://<ip>:8000/v1/devices/add"
 
+def main():
+    input_data = load_input_json(input_json)
+    url = f"http://{server_ip}:{server_port}/"
+
+    method = input_data["method"]
     payload = input_data["data"]
 
-    response_data = send_request(payload, url)
-    save_output_json(response_data, "output.json")
+    response_data = send_request(payload, url + method)
+    save_output_json(response_data, output_json)
+
 
 if __name__ == "__main__":
     main()
--- http_server.py
+++ http_server.py
@@ -1,25 +1,36 @@
-import os
 import json
+import sys
+
 import pandas as pd
-from sanic import Sanic, response
+from sanic import Sanic
 from sanic.request import Request
 from sanic.response import json
 
+server_port = int(sys.argv[1])
+db_file = sys.argv[2]
+
 app = Sanic("DeviceAPI")
 
-csv_file = "devices.csv"
 columns = [
-    "id", "name", "type",
-    "hardware_model", "hardware_sn",
-    "software_version", "software_last_update",
-    "nic1_type", "nic1_mac", "nic1_ipv4",
-    "nic2_type", "nic2_mac", "nic2_ipv4",
-    "state"
+    "id",
+    "name",
+    "type",
+    "hardware_model",
+    "hardware_sn",
+    "software_version",
+    "software_last_update",
+    "nic1_type",
+    "nic1_mac",
+    "nic1_ipv4",
+    "nic2_type",
+    "nic2_mac",
+    "nic2_ipv4",
+    "state",
 ]
 
-if not os.path.exists(csv_file):
-    df = pd.DataFrame(columns=columns)
-    df.to_csv(csv_file, index=False)
+# if not os.path.exists(db_file):
+#     df = pd.DataFrame(columns=columns)
+#     df.to_csv(db_file, index=False)
 
 
 def flatten_device(device):
@@ -27,24 +38,56 @@ def flatten_device(device):
         **{k: device[k] for k in ("id", "name", "type", "state")},
         **{f"hardware_{k}": v for k, v in device["hardware"].items()},
         **{f"software_{k}": v for k, v in device["software"].items()},
-        **{f"nic{i+1}_{k}": v for i, nic in enumerate(device["nic"][:2]) for k, v in nic.items()}
+        **{
+            f"nic{i+1}_{k}": v
+            for i, nic in enumerate(device["nic"][:2])
+            for k, v in nic.items()
+        },
     }
+
 
 @app.post("/v1/devices/add")
 async def add_device(request: Request):
     device = request.json
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(db_file)
 
     if device["id"] in df["id"].values:
         return json({"status": 400, "msg": "Device ID already exists."}, status=400)
-    
-    # Flatten device data
+
     device = flatten_device(device)
-
     df = df.append(device, ignore_index=True)
-    df.to_csv(csv_file, index=False)
+    df.to_csv(db_file, index=False)
 
-    return json({"status": 200, "msg": "Device added successfully."})
+    return json({"status": "success", "data": None})
+
+
+@app.delete("/v1/devices/DELETE")
+async def delete_device(request: Request):
+    device_ids = request.json["data"]
+    df = pd.read_csv(db_file)
+
+    df = df[~df["id"].isin(device_ids)]
+    df.to_csv(db_file, index=False)
+
+    return json({"status": "success", "data": None})
+
+
+@app.post("/v1/devices/QUERY")
+async def query_device(request: Request):
+    device_ids = request.json["data"]
+    df = pd.read_csv(db_file)
+
+    queried_devices = df[df["id"].isin(device_ids)].to_dict(orient='records')
+
+    return json({"status": "success", "data": queried_devices})
+
+
+@app.post("/v1/devices/QUERYALL")
+async def query_all_devices():
+    df = pd.read_csv(db_file)
+    all_devices = df.to_dict(orient='records')
+    return json({"status": "success", "data": all_devices})
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=server_port)
